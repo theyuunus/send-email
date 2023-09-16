1253a41648b3a7dad8e0e94e6b2facc93b5e3413
Yunus <128239091+theyuunus@users.noreply.github.com>
2023-09-16 16:16:53 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { ContactUs } from "./components/contact";
+import "./components/contact.scss"
+
+function App() {
+  return (
+    <div className="App">
+      <ContactUs />
+    </div>
+  );
+}
+
+export default App;
